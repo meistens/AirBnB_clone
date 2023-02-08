7e18ad5f48be87f6476c87b477ec3f2a05003719
David Mebo <mebodave@gmail.com>
2023-02-08 20:49:45 +0100

    tested code (not unittest)

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,6 @@ class BaseModel:
         # returns a dictionary containing all key/value pairs of __dict__ of the instance
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        new_dict.update({"created_at": self.created_at.isoformat()})
-        new_dict.update({"updated_at": self.updated_at.isoformat()})
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
