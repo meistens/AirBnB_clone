71da227c997307a971b097bb15ff895688b4dcd8
David Mebo <mebodave@gmail.com>
2023-02-09 01:51:18 +0100

    fixed docstrings

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Defines the modules used for testing"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+import json
+
+class BaseModel_test(unittest.TestCase):
+    """test class for the BaseModel class"""
+    def test_doc(self):
+        """checks if documentation exists"""
+        docs = BaseModel.__init__.__doc__
+        self.assertGreater(len(docs), 1)
+
+    def test_datetime(self):
+        # compares the created_at and updated_at dates (should not be the same)
+       created_at_date = BaseModel()
+       updated_at_date = BaseModel()
+       self.assertNotEqual(created_at_date.created_at, updated_at_date.updated_at)
+       self.assertNotEqual(updated_at_date.created_at, created_at_date.updated_at)
