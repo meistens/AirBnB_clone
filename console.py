5b3b49eff38b64cb48615ed3db4f2971d09b87e0
David Mebo <mebodave@gmail.com>
2023-02-10 19:44:02 +0100

    show command added for the interpreter

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Import modules"""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Class for the HBNB interpreter"""
@@ -19,5 +21,39 @@ class HBNBCommand(cmd.Cmd):
         """ENTER and emptyline should not execute anything"""
         pass
 
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it to the given JSON file and prints its id"""
+        class_name = args.strip()
+        if not args:
+            print("** class name missing **")
+            return
+        elif class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        else:
+            new_model = BaseModel()
+            new_model.save()
+            print(new_model.id)
+
+    def do_show(self, args):
+        """Prints the string representation of an instance based on its class name and id"""
+        if not args:
+            print("** class name missing **")
+            return
+        args = args.split()
+        if not len(args):
+            print("** instance id missing **")
+            return
+        class_name = args[0]
+        if class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        key = "{}.{}".format(class_name, args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+        
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
