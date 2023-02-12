659748e2902aaf35ed34449df4924fe50b3e706f
David Mebo <mebodave@gmail.com>
2023-02-12 22:58:28 +0100

    interpreter complete

--- console.py
+++ console.py
@@ -4,6 +4,12 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,6 +20,11 @@ class HBNBCommand(cmd.Cmd):
     __classes = {
         "BaseModel",
         "User",
+        "Amenity",
+        "City",
+        "Place",
+        "Review",
+        "State"
     }
 
     def do_quit(self, line):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,11 @@
 from models.base_model import BaseModel
 import json
 from models.user import User
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 class FileStorage():
     """A private class for the serialization and deserialization class of"\
